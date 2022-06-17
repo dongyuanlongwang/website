ec8591f601fdc278015836b72247f1be8b7b09b8
赵睿 <zhaoruizzb@163.com>
2022-06-17 20:37:17 +0800

    给刘奇总的简单版本

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,77 +1,77 @@
 <template>
+  <img style="height: 100% ;width: 100%" src="@/assets/img/img.png" alt="big1"/>
+<!--  <el-carousel :interval="4000" indicator-position="outside" type="card" height="450px">-->
+<!--    <el-carousel-item>-->
+<!--      <img style="height: 100% ;width: 100%" src="@/assets/img/mock/big1.jpg" alt="big1"/>-->
+<!--    </el-carousel-item>-->
+<!--    <el-carousel-item>-->
+<!--      <img style="height: 100% ;width: 100%" src="@/assets/img/mock/big2.jpg" alt="big1"/>-->
+<!--    </el-carousel-item>-->
+<!--    <el-carousel-item>-->
+<!--      <img style="height: 100% ;width: 100%" src="@/assets/img/mock/big3.jpg" alt="big1"/>-->
+<!--    </el-carousel-item>-->
+<!--  </el-carousel>-->
+<!--  <el-row :gutter="24">-->
+<!--    <el-col :span="18">-->
+<!--      <div class="red-back">-->
+<!--        <div class="pad">-->
+<!--          <div class="ticket-report">-->
+<!--            <h3>票务通知</h3>-->
+<!--            <el-icon>-->
+<!--              <ArrowLeftBold/>-->
+<!--            </el-icon>-->
+<!--          </div>-->
+<!--          <h5 style="color: white">-->
+<!--            观众可提前3日（含当天）通过“山西博物院”微信公众号点击“参观服务”实名预约，每个预约单最多预约3人（每人都需要提供个人信息），每张身份证每天限约1次，请务必保存好预约凭证。</h5>-->
+<!--        </div>-->
 
-  <el-carousel :interval="4000" indicator-position="outside" type="card" height="450px">
-    <el-carousel-item>
-      <img style="height: 100% ;width: 100%" src="@/assets/img/mock/big1.jpg" alt="big1"/>
-    </el-carousel-item>
-    <el-carousel-item>
-      <img style="height: 100% ;width: 100%" src="@/assets/img/mock/big2.jpg" alt="big1"/>
-    </el-carousel-item>
-    <el-carousel-item>
-      <img style="height: 100% ;width: 100%" src="@/assets/img/mock/big3.jpg" alt="big1"/>
-    </el-carousel-item>
-  </el-carousel>
-  <el-row :gutter="24">
-    <el-col :span="18">
-      <div class="red-back">
-        <div class="pad">
-          <div class="ticket-report">
-            <h3>票务通知</h3>
-            <el-icon>
-              <ArrowLeftBold/>
-            </el-icon>
-          </div>
-          <h5 style="color: white">
-            观众可提前3日（含当天）通过“山西博物院”微信公众号点击“参观服务”实名预约，每个预约单最多预约3人（每人都需要提供个人信息），每张身份证每天限约1次，请务必保存好预约凭证。</h5>
-        </div>
+<!--        <div class="pad2">-->
+<!--          <h2>免费开放时间</h2>-->
+<!--          <div>-->
+<!--            <el-row gutter="24">-->
+<!--              <el-col :span="8">-->
+<!--                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">-->
+<!--                <div class="date">09:00<span class="s1">开馆</span>-->
+<!--                </div>-->
+<!--                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">-->
+<!--              </el-col>-->
+<!--              <el-col :span="8">-->
+<!--                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">-->
+<!--                <div class="date">09:00<span class="s1">开馆</span>-->
+<!--                </div>-->
+<!--                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">-->
+<!--              </el-col>-->
+<!--              <el-col :span="8">-->
+<!--                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">-->
+<!--                <div class="date">09:00<span class="s1">开馆</span>-->
+<!--                </div>-->
+<!--                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">-->
+<!--              </el-col>-->
+<!--            </el-row>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </el-col>-->
+<!--    <el-col :span="6">-->
+<!--      <div class="block-col">-->
+<!--        <el-row :gutter="24">-->
+<!--          <el-col style="padding:10% 0" :span="8">-->
+<!--            <Service style="width: 3em; height: 3em; margin-right: 8px"/>-->
+<!--            <h3>开放服务</h3>-->
+<!--          </el-col>-->
+<!--          <el-col style="padding:10% 0" :span="8">-->
+<!--            <Guide style="width: 3em; height: 3em; margin-right: 8px"/>-->
+<!--            <h3>参观导览</h3>-->
+<!--          </el-col>-->
+<!--          <el-col style="padding:10% 0" :span="8">-->
+<!--            <Comment style="width: 3em; height: 3em; margin-right: 8px"/>-->
+<!--            <h3>加入我们</h3>-->
+<!--          </el-col>-->
+<!--        </el-row>-->
+<!--      </div>-->
 
-        <div class="pad2">
-          <h2>免费开放时间</h2>
-          <div>
-            <el-row gutter="24">
-              <el-col :span="8">
-                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">
-                <div class="date">09:00<span class="s1">开馆</span>
-                </div>
-                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">
-              </el-col>
-              <el-col :span="8">
-                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">
-                <div class="date">09:00<span class="s1">开馆</span>
-                </div>
-                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">
-              </el-col>
-              <el-col :span="8">
-                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">
-                <div class="date">09:00<span class="s1">开馆</span>
-                </div>
-                <img style="width: 100%" src="@/assets/img/mock/arr5.png" alt="">
-              </el-col>
-            </el-row>
-          </div>
-        </div>
-      </div>
-    </el-col>
-    <el-col :span="6">
-      <div class="block-col">
-        <el-row :gutter="24">
-          <el-col style="padding:10% 0" :span="8">
-            <Service style="width: 3em; height: 3em; margin-right: 8px"/>
-            <h3>开放服务</h3>
-          </el-col>
-          <el-col style="padding:10% 0" :span="8">
-            <Guide style="width: 3em; height: 3em; margin-right: 8px"/>
-            <h3>参观导览</h3>
-          </el-col>
-          <el-col style="padding:10% 0" :span="8">
-            <Comment style="width: 3em; height: 3em; margin-right: 8px"/>
-            <h3>加入我们</h3>
-          </el-col>
-        </el-row>
-      </div>
-
-    </el-col>
-  </el-row>
+<!--    </el-col>-->
+<!--  </el-row>-->
 
 </template>
 <script setup>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,52 +1,54 @@
 <template>
   <el-container>
-    <el-header>
-      <el-menu :default-active="activeIndex"
-               class="el-menu-demo"
-               mode="horizontal"
-               :ellipsis="false"
-               @select="handleSelect">
-        <el-menu-item index="0">
-          <img alt="logo" style="width: 50% ; height: 50%" src="@/assets/img/logo.png"/>
-        </el-menu-item>
-        <div class="flex-grow"/>
-        <el-menu-item index="home">首页</el-menu-item>
-        <el-sub-menu index="about">
-          <template #title>关于烽博</template>
-          <el-menu-item index="about">烽博介绍</el-menu-item>
-          <el-menu-item index="org">组织机构</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="3">
-          <template #title>最新动态</template>
-          <el-menu-item index="3-1">最新动态</el-menu-item>
-          <el-menu-item index="3-2">公示公告</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="4">
-          <template #title>馆藏文物</template>
-          <el-menu-item index="4-1">文物鉴赏</el-menu-item>
-          <el-menu-item index="4-2">文物征集</el-menu-item>
-          <el-menu-item index="4-3">展览陈列</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="5">
-          <template #title>服务</template>
-          <el-menu-item index="5-1">参观指南</el-menu-item>
-          <el-menu-item index="5-2">失物招领</el-menu-item>
-          <el-menu-item index="5-2">网游留言</el-menu-item>
-          <el-menu-item index="5-3">教育活动</el-menu-item>
-          <el-menu-item index="5-3">志愿者之家</el-menu-item>
-        </el-sub-menu>
-        <el-sub-menu index="6">
-          <template #title>参与互动</template>
-          <el-menu-item index="6-1">参观指南</el-menu-item>
-          <el-menu-item index="6-2">失物招领</el-menu-item>
-        </el-sub-menu>
-        <el-menu-item index="4">研究创新</el-menu-item>
-      </el-menu>
-    </el-header>
+<!--    <el-header>-->
+<!--      <el-menu :default-active="activeIndex"-->
+<!--               class="el-menu-demo"-->
+<!--               mode="horizontal"-->
+<!--               :ellipsis="false"-->
+<!--               @select="handleSelect">-->
+<!--        <el-menu-item index="0">-->
+<!--          <img alt="logo" style="width: 50% ; height: 50%" src="@/assets/img/logo.png"/>-->
+<!--        </el-menu-item>-->
+<!--        <div class="flex-grow"/>-->
+<!--        <el-menu-item index="home">首页</el-menu-item>-->
+<!--        <el-sub-menu index="about">-->
+<!--          <template #title>关于烽博</template>-->
+<!--          <el-menu-item index="about">烽博介绍</el-menu-item>-->
+<!--          <el-menu-item index="org">组织机构</el-menu-item>-->
+<!--        </el-sub-menu>-->
+<!--        <el-sub-menu index="3">-->
+<!--          <template #title>最新动态</template>-->
+<!--          <el-menu-item index="3-1">最新动态</el-menu-item>-->
+<!--          <el-menu-item index="3-2">公示公告</el-menu-item>-->
+<!--        </el-sub-menu>-->
+<!--        <el-sub-menu index="4">-->
+<!--          <template #title>馆藏文物</template>-->
+<!--          <el-menu-item index="4-1">文物鉴赏</el-menu-item>-->
+<!--          <el-menu-item index="4-2">文物征集</el-menu-item>-->
+<!--          <el-menu-item index="4-3">展览陈列</el-menu-item>-->
+<!--        </el-sub-menu>-->
+<!--        <el-sub-menu index="5">-->
+<!--          <template #title>服务</template>-->
+<!--          <el-menu-item index="5-1">参观指南</el-menu-item>-->
+<!--          <el-menu-item index="5-2">失物招领</el-menu-item>-->
+<!--          <el-menu-item index="5-2">网游留言</el-menu-item>-->
+<!--          <el-menu-item index="5-3">教育活动</el-menu-item>-->
+<!--          <el-menu-item index="5-3">志愿者之家</el-menu-item>-->
+<!--        </el-sub-menu>-->
+<!--        <el-sub-menu index="6">-->
+<!--          <template #title>参与互动</template>-->
+<!--          <el-menu-item index="6-1">参观指南</el-menu-item>-->
+<!--          <el-menu-item index="6-2">失物招领</el-menu-item>-->
+<!--        </el-sub-menu>-->
+<!--        <el-menu-item index="4">研究创新</el-menu-item>-->
+<!--      </el-menu>-->
+<!--    </el-header>-->
     <el-main>
       <router-view/>
     </el-main>
-    <el-footer style="position: fixed; bottom: 0">晋ICP备08001524 联系电话：03518789188、03518789222</el-footer>
+    <el-footer style="position: fixed; bottom: 50%">
+      <h2 >联系邮箱  liuqi@luganghui.com</h2>
+    </el-footer>
   </el-container>
 </template>
 
