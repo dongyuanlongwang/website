fd28734a11b4c0809acc16046d88a62510aa8c92
赵睿 <zhaoruizzb@163.com>
2022-09-05 10:44:32 +0800

    增加自适应支持

--- src/views/home.vue
+++ src/views/home.vue
@@ -9,35 +9,65 @@ import eshop from '@/assets/img/pexels-fauxels-3184464.jpg'
 
 
 <template>
-  <el-image :src="port" lazy/>
-  <div class="text" style="top: 15%;  right: 3%;">
-    <h2>Port services</h2>
-    <h3>
-      Port service market refers to the market for trading port services. It has obvious regional characteristics. If there is only one available port in a given area, then the port market in that area is a complete seller's monopoly market. In most cases, there are several ports at the same time to provide homogeneous services for the import and export of goods in the hinterland.
-    </h3>
+  <div class="img">
+    <el-image class="img" :src="port" lazy>
+      <template #placeholder>
+        <div class="image-slot">Loading<span class="dot">...</span></div>
+      </template>
+    </el-image>
+    <div  class="text" style="top: -70vh;  left: 77vw;">
+      <h2>Port services</h2>
+      <h3>
+        Port service market refers to the market for trading port services. It has obvious regional characteristics. If
+        there is only one available port in a given area, then the port market in that area is a complete seller's
+        monopoly market. In most cases, there are several ports at the same time to provide homogeneous services for the
+        import and export of goods in the hinterland.
+      </h3>
+    </div>
   </div>
-  <el-image :src="port_6670684" lazy/>
-  <div class="text" style="top: 150%; left: 3%;">
-    <h2>Maritime transport</h2>
-    <h3>
-      Our shipping FCL service covers the world's main routes, we provide ordinary containers, freezers, special containers and other transportation modes, in the premise of providing you with reliable shipping space, so that you have more choices. Our professional team will suggest a reasonable transportation route according to your needs.
-    </h3>
+  <div class="img">
+    <el-image class="img" :src="port_6670684" lazy>
+      <template #placeholder>
+        <div class="image-slot">Loading<span class="dot">...</span></div>
+      </template>
+    </el-image>
+    <div class="text" style="top: -80vh; left: 3vw;">
+      <h2>Maritime transport</h2>
+      <h3>
+        Our shipping FCL service covers the world's main routes, we provide ordinary containers, freezers, special
+        containers and other transportation modes, in the premise of providing you with reliable shipping space, so that
+        you have more choices. Our professional team will suggest a reasonable transportation route according to your
+        needs.
+      </h3>
+    </div>
   </div>
-
-  <el-image :src="rice" lazy/>
-  <div class="text" style="top: 300%; right:3%;">
-    <h2>Trade in bulk goods</h2>
-    <h3>
-      Bulk grain import and export, covering barley, wheat, corn, sugar soybeans, fertilizer and other metals and raw materials import and export. Recycled copper, lithium phosphate, oil and natural gas. Consumer electronics.
-    </h3>
+  <div class="img">
+    <el-image class="img" :src="rice" lazy>
+      <template #placeholder>
+        <div class="image-slot">Loading<span class="dot">...</span></div>
+      </template>
+    </el-image>
+    <div class="text" style="top: -80vh; left:77vw;">
+      <h2>Trade in bulk goods</h2>
+      <h3>
+        Bulk grain import and export, covering barley, wheat, corn, sugar soybeans, fertilizer and other metals and raw
+        materials import and export. Recycled copper, lithium phosphate, oil and natural gas. Consumer electronics.
+      </h3>
+    </div>
   </div>
-
-  <el-image :src="eshop" lazy/>
-  <div class="text" style="top: 430%; left: 40%;">
-    <h2>Cross-boundary Electronic Accounts</h2>
-    <h3>
-      Its products include consumer electronics and home furnishings, which reach the global market through platforms such as Amazon.
-    </h3>
+  <div class="img">
+      <el-image class="img" :src="eshop" lazy>
+        <template #placeholder>
+          <div class="image-slot">Loading<span class="dot">...</span></div>
+        </template>
+      </el-image>
+      <div class="text" style="top: -60vh; left: 40vw;">
+        <h2>Cross-boundary Electronic Accounts</h2>
+        <h3>
+          Its products include consumer electronics and home furnishings, which reach the global market through platforms
+          such as Amazon.
+        </h3>
+      </div>
   </div>
 
   <div class="connect">
@@ -69,13 +99,13 @@ import eshop from '@/assets/img/pexels-fauxels-3184464.jpg'
 
 <style lang="scss" scoped>
 .text {
-  position: inherit;
-  width: 20%;
+  position: relative;
+  width: 20vw;
   color: #f2f3f5;
 }
 
 .connect {
-  height: 50%;
+  height: 50vh;
   color: #000000;
   background-color: rgb(199, 78, 36);
 }
@@ -92,6 +122,12 @@ import eshop from '@/assets/img/pexels-fauxels-3184464.jpg'
   display: flex;
   justify-content: end;
   align-items: center;
+}
+
+.img {
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
 }
 
 .right {
