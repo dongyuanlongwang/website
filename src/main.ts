2ff75b6b8b5a288277111c315dc064cc68fd114a
赵睿 <zhaoruizzb@163.com>
2023-09-01 14:46:22 +0800

    直到现在才能做出最简单的版本

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,14 @@
-import {createApp} from "vue";
+import { createApp } from "vue";
 
 import App from './App.vue'
 
 import router from './router/index'
 
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-import 'element-plus/dist/index.css'
-// import './assets/styles/element.scss'
 
 import './index.css'
 
 const app = createApp(App)
-
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
-
-
-app.use(router)
-
-// 使用element-plus 并且设置全局的大小
-app.use(ElementPlus, {
-  locale: zhCn,
-})
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
 
 
 app.mount('#app')
